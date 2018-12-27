6913706fb95b865f925caedea7e86c05a188c2f4
Victor Mikhalev <v.mikhalev@pixlpark.com>
2018-12-27 15:07:08 +0700

    memory

---
+++ src/Competition/Benchmarks.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections.Generic;
+using System.Text;
+using BenchmarkDotNet.Attributes;
+using Pixlpark.Text;
+
+namespace Competition
+{
+    [MemoryDiagnoser]
+    public class Benchmarks
+    {
+        private const int IterationsCount = 1_000;
+        private static IReadOnlyDictionary<string, string> ReplacementData;
+        private static string InputData;
+
+        [GlobalSetup]
+        public void GlobalSetup()
+        {
+            var str = System.IO.File.ReadAllText(System.Environment.CurrentDirectory + "\\data\\replacements.json", Encoding.UTF8);
+            ReplacementData = Newtonsoft.Json.JsonConvert.DeserializeObject<Dictionary<string, string>>(str);
+            InputData = System.IO.File.ReadAllText(System.Environment.CurrentDirectory + "\\data\\input1.html", Encoding.UTF8);
+        }
+
+        [Benchmark]
+        public static void RunStandardReplacer()
+        {
+            for (int i = 0; i < IterationsCount; i++)
+            {
+                StandardReplacer.Replace(InputData, token =>
+                {
+                    var t = "[$" + token + "$]";
+                    return ReplacementData[t];
+                });
+            }
+        }
+
+        [Benchmark]
+        public static void RunStupidReplacer()
+        {
+            for (int i = 0; i < IterationsCount; i++)
+            {
+                StupidReplacer.Replace(InputData, token =>
+                {
+                    var t = "[$" + token + "$]";
+                    return ReplacementData[t];
+                });
+            }
+        }
+    }
+}
--- src/Competition/Program.cs
+++ src/Competition/Program.cs
@@ -1,62 +1,15 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
-using BenchmarkDotNet.Attributes;
 using BenchmarkDotNet.Running;
-using Pixlpark.Text;
 
 namespace Competition
 {
     public class Program
     {
-        private const int IterationsCount = 10_000;
-        private static IReadOnlyDictionary<string, string> ReplacementData;
-        private static string InputData;
         public static void Main(string[] args)
         {
-            var str = System.IO.File.ReadAllText(System.Environment.CurrentDirectory + "\\data\\replacements.json", Encoding.UTF8);
-            ReplacementData = Newtonsoft.Json.JsonConvert.DeserializeObject<Dictionary<string, string>>(str);
-            InputData = System.IO.File.ReadAllText(System.Environment.CurrentDirectory + "\\data\\input1.html", Encoding.UTF8);
-
-            BenchmarkRunner.Run<Program>();
-
+            BenchmarkRunner.Run<Benchmarks>();
+            Console.WriteLine("Benchmark complete. Press any key to close...");
             Console.ReadKey();
         }
-
-        [GlobalSetup]
-        public void GlobalSetup()
-        {
-            var str = System.IO.File.ReadAllText(System.Environment.CurrentDirectory + "\\data\\replacements.json", Encoding.UTF8);
-            ReplacementData = Newtonsoft.Json.JsonConvert.DeserializeObject<Dictionary<string, string>>(str);
-            InputData = System.IO.File.ReadAllText(System.Environment.CurrentDirectory + "\\data\\input1.html", Encoding.UTF8);
-        }
-
-        [Benchmark]
-        public static void RunStandardReplacer()
-        {
-            for (int i = 0; i < IterationsCount; i++)
-            {
-                StandardReplacer.Replace(InputData, token =>
-                {
-                    var t = "[$" + token + "$]";
-                    return ReplacementData[t];
-                });
-            }
-        }
-
-        [Benchmark]
-        public static void RunStupidReplacer()
-        {
-            for (int i = 0; i < IterationsCount; i++)
-            {
-                StupidReplacer.Replace(InputData, token =>
-                {
-                    var t = "[$" + token + "$]";
-                    return ReplacementData[t];
-                });
-            }
-        }
-
-
     }
 }
